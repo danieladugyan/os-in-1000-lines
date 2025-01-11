3b9ec3bc1e9cb36e27081dd5bfdff4b7429949e9
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-01-11 14:25:26 +0100

    Add memory allocation

--- kernel.c
+++ kernel.c
@@ -113,16 +113,32 @@ void handle_trap(struct trap_frame *f) {
           user_pc);
 }
 
+extern char __free_ram[], __free_ram_end[];
+
+// Bump/linear allocator
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t)__free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t)__free_ram_end) {
+        PANIC("out of memory");
+    }
+
+    memset((void *)paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 void kernel_main(void) {
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
     WRITE_CSR(stvec, (uint32_t)kernel_entry);
 
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    PANIC("booted!");
 }
 
 __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
